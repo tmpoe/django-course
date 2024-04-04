e2f90e72b6106dc248af0025c1531cfd424e6118
tmpoe <torok.mark.patrik@gmail.com>
2024-04-04 07:48:40 +0200

    Finis section 5

--- food-site/mysite/food/views.py
+++ food-site/mysite/food/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the food index.")
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
 
 def item(request, item_id):
     return HttpResponse("You're looking at item %s." % item_id)
