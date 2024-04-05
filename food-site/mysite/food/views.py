8d9628f21698ad652418c6c2a69ae2e65c957648
tmpoe <torok.mark.patrik@gmail.com>
2024-04-05 18:44:40 +0200

    Refactor

--- food-site/mysite/food/views.py
+++ food-site/mysite/food/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
-from django.template import loader
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    template = loader.get_template('food/index.html')
-    return HttpResponse(template.render(context={'item_list': item_list}, request=request))
+    context = {
+        'item_list': item_list,
+    }
+    return render(context,'food/index.html', request)
 
 def item(request, item_id):
     return HttpResponse("You're looking at item %s." % item_id)
