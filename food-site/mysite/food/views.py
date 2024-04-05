af8180d3efac895db267cd94584f390d86129b2d
tmpoe <torok.mark.patrik@gmail.com>
2024-04-05 18:38:41 +0200

    Add rough template

--- food-site/mysite/food/views.py
+++ food-site/mysite/food/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
+from django.template import loader
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    template = loader.get_template('food/index.html')
+    return HttpResponse(template.render(context={'item_list': item_list}, request=request))
 
 def item(request, item_id):
     return HttpResponse("You're looking at item %s." % item_id)
