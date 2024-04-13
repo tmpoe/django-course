ca0edf3c879db263f69c3896c8953bb5a76a890a
tmpoe <torok.mark.patrik@gmail.com>
2024-04-13 09:37:02 +0200

    Finish section 8

--- food-site/mysite/food/urls.py
+++ food-site/mysite/food/urls.py
@@ -6,4 +6,7 @@ app_name = 'food'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.item, name='item'),
+    path('add', views.add_item, name='add_item'),
+    path('edit/<int:item_id>/', views.edit_item, name='edit_item'),
+    path('delete/<int:item_id>/', views.delete_item, name='delete_item')
 ]
--- food-site/mysite/food/views.py
+++ food-site/mysite/food/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
@@ -11,6 +12,34 @@ def index(request):
     }
     return render(request,'food/index.html', context)
 
-def item(request, item_id):
+def item(request, item_id: int):
     item = get_object_or_404(Item, pk=item_id)
     return render(request, 'food/item.html', {'item': item})
+
+def add_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item_form.html', {'form': form})
+
+def edit_item(request, item_id: int):
+    item = Item.objects.get(id=item_id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item_form.html', {'form': form})
+
+def delete_item(request, item_id: int):
+    item = Item.objects.get(id=item_id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request, 'food/item_delete.html', {'item': item})
