2308556a3dcf4075c412e812014ff2af6f9470a0
tmpoe <torok.mark.patrik@gmail.com>
2024-04-15 07:30:34 +0200

    Add basic registration form

---
+++ food-site/mysite/users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+# Create your views here.
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}! Your account has been created.')
+            return redirect('food:index')
+    else:
+        form = UserCreationForm()
+        print(form)
+    return render(request, 'users/register.html', {'form': form})
